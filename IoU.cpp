37cf9177685a7a131371ceb01a8e0176e6abae2d
roflan635 <nikolaev.nikita2281337@gmail.com>
2025-03-30 19:16:31 +0400

    пока осознаю как и че

--- IoU.cpp
+++ IoU.cpp
@@ -1,7 +1,19 @@
 ﻿#include <iostream>
 
+struct Rect
+{
+    int x1, y1; // координаты верхнего левого угла области
+    int x2, y2; // координаты нижнего правого угла области
+};
+
+double IoU(const Rect& region1, const Rect& region2)
+{
+    // ... Разместите здесь свой код
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Rect r1{ 0,0,30,40 }, r2{ 15,15,120,25 };
+    double result1 = IoU(Rect{ 0,0,5,5 }, Rect{ 2,2,3,3, });
 }
 
