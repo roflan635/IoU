b976f1b5ced89e876046d19c6420c87c3b7ed95c
roflan635 <nikolaev.nikita2281337@gmail.com>
2025-03-31 18:54:08 +0400

    много условий надо придумывать. (┬┬﹏┬┬)

--- IoU.cpp
+++ IoU.cpp
@@ -8,12 +8,22 @@ struct Rect
 
 double IoU(const Rect& region1, const Rect& region2)
 {
-    // ... Разместите здесь свой код
+    double square1, square2, smallSquare, smallX, smallY, generalSquare, result;
+    square1 = (region1.x2 - region1.x1) * (region1.y2 - region1.y1);
+    square2 = (region2.x2 - region2.x1) * (region2.y2 - region2.y1);
+    if (region1.x2 < region2.x1 && region1.y2 < region2.y1) //проверка на пересечение областей
+        return -1;
+    else if (region1.x1)
+    //return result = smallSquare / generalSquare;
 }
 
 int main()
 {
-    Rect r1{ 0,0,30,40 }, r2{ 15,15,120,25 };
-    double result1 = IoU(Rect{ 0,0,5,5 }, Rect{ 2,2,3,3, });
+    //region1 - объект
+    //region2 - фото нейросети
+    //Rect r1{ 0,0,30,40 }, r2{ 15,15,120,25 };
+    //double result1 = IoU(Rect{ 0,0,5,5 }, Rect{ 2,2,3,3});
+    //double result1 = IoU(Rect{ 0,0,1,1 }, Rect{ 2,2,3,3 });
+    double result1 = IoU(Rect{ 0,0,1,1 }, Rect{ 2,2,6,6 });
+    std::cout << result1 << std::endl;
 }
-
