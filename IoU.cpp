3e39929e3b819b28d32d30c3002b17b4e4f07cec
roflan635 <148633870+roflan635@users.noreply.github.com>
2025-04-21 14:03:28 +0300

    Update IoU.cpp
    ниче не работает, я устал

--- IoU.cpp
+++ IoU.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 struct Rect
 {
@@ -14,10 +14,27 @@ double IoU(const Rect& region1, const Rect& region2)
         return -1;
     square1 = (region1.x2 - region1.x1) * (region1.y2 - region1.y1);
     square2 = (region2.x2 - region2.x1) * (region2.y2 - region2.y1);
-    small.x1 = region2.x1 - region1.x1;
-    small.y1 = region1.y1 + region2.y1;
-    small.x2 = region2.x2;
-    small.y2 = region1.y2;
+    if (region2.x1 <= region1.x1 && region2.y1 <= region1.y1)
+    {
+        small.x1 = region2.x1; 
+        small.y1 = region2.y1; 
+        small.x2 = region2.x2; 
+        small.y2 = region1.y2;
+    }
+    else if (region2.x1 >= region1.x1 && region2.y1 >= region1.y1)
+    {
+        small.x1 = region1.x1; 
+        small.y1 = region1.y1; 
+        small.x2 = region2.x2; 
+        small.y2 = region1.y2;
+    }
+    else if (region2.x1 >= region1.x1 && region2.y1 >= region1.y1 || region2.x1 <= region1.x1 && region2.y1 <= region1.y1)
+    {
+        small.x1 = region2.x1 - region1.x1; 
+        small.y1 = region1.y1 + region2.y1; 
+        small.x2 = region2.x2; 
+        small.y2 = region1.y2;
+    }
     smallSquare = (small.x2 - small.x1) * (small.y2 - small.y1);
     generalSquare = square1 + square2 - smallSquare;
     return result = smallSquare / generalSquare;
@@ -30,6 +47,6 @@ int main()
     //Rect r1{ 0,0,30,40 }, r2{ 15,15,120,25 };
     //double result1 = IoU(Rect{ 0,0,5,5 }, Rect{ 2,2,3,3});
     //double result1 = IoU(Rect{ 0,0,1,1 }, Rect{ 2,2,3,3 });
-    double result1 = IoU(Rect{ 0,3,10,8 }, Rect{ 5,0,8,10 });
+    double result1 = IoU(Rect{ 2,1,4,4 }, Rect{ 2,3,6,6 });
     std::cout << result1 << std::endl;
 }
